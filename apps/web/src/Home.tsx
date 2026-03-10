456e6bcae7712ae499adab60a1f2451218224483
Brandon <95515854+brandon-kong@users.noreply.github.com>
2026-03-10 16:53:03 -0500

    feat: added custom useApi/useLazyApi hooks

--- apps/web/src/Home.tsx
+++ apps/web/src/Home.tsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { useState } from 'react';
-import { generateMagicLink } from '@auth/api-client';
+import { generateMagicLink, useLazyApi } from '@auth/api-client';
 
 function Home() {
-    const [email, setEmail] = useState<string>("");
+     const [email, setEmail] = useState<string>("");
+    const { refetch, loading, response } = useLazyApi(() => generateMagicLink(email), [email]);
 
-  return <div style={{
-    display: 'flex',
-    justifyContent:'center',
-    alignItems:'center'
-  }}>
-    <h1>Welcome to react router dom and Query!</h1>
-    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-    <button
-    onClick={() => generateMagicLink(email)}
-    >
-        Test
-    </button>
-  </div>;
+     return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        { loading ? "Loading" : undefined }
+        <h1>Welcome to react router dom and Query!</h1>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <button
+        onClick={refetch}
+        >
+            Test
+        </button>
+        { JSON.stringify(response) }
+      </div>;
 }
 
 export default Home;
---
+++ packages/api-client/src/lib/api.ts
@@ -0,0 +1,68 @@
+import { useState, useEffect, useCallback } from "react";
+import type { ApiResponse } from "../types/response";
+import { HTTPError } from "ky";
+
+export interface ExtendedApiResponse<T extends {} = {}> {
+    loading: boolean;
+    fetchError: string | undefined;
+    response: ApiResponse<T> | undefined;
+    refetch: () => Promise<void>;
+}
+
+export function useApi<T extends {} = {}>(
+    callback: () => Promise<ApiResponse<T>>
+): ExtendedApiResponse<T> {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [fetchError, setFetchError] = useState<string | undefined>(undefined);
+    const [response, setResponse] = useState<ApiResponse<T> | undefined>(undefined);
+
+    const fetch = async () => {
+        setLoading(true);
+        setFetchError(undefined);
+        try {
+            const res = await callback();
+            setResponse(res);
+        } catch (err) {
+            setFetchError(err instanceof Error ? err.message : "Unknown error");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
+    return { loading, fetchError, response, refetch: fetch };
+}
+
+export function useLazyApi<T extends {} = {}>(
+    callback: () => Promise<ApiResponse<T>>,
+    dependencies?: any[]
+): ExtendedApiResponse<T> {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [fetchError, setFetchError] = useState<string | undefined>(undefined);
+    const [response, setResponse] = useState<ApiResponse<T> | undefined>(undefined);
+
+    const fetch = useCallback(async () => {
+        setLoading(true);
+        setFetchError(undefined);
+        try {
+            const res = await callback();
+            setResponse(res);
+        } catch (err) {
+            if (err instanceof HTTPError) {
+                const errorResponse = await err.response.json<ApiResponse<T>>();
+                setResponse(errorResponse)
+            }
+            else {
+                setFetchError(err instanceof Error ? err.message : "Unknown error");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }, dependencies ?? []);
+
+    return { loading, fetchError, response, refetch: fetch };
+}
+

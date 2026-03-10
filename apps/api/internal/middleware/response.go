d84427a1f6c8872b5f7bd00ef003ead70c925ce5
Brandon <95515854+brandon-kong@users.noreply.github.com>
2026-03-10 01:16:06 -0500

    feat: added JSON response helper

--- apps/api/internal/middleware/response.go
+++ apps/api/internal/middleware/response.go
@@ -1,8 +1,18 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func WriteResponse(w http.ResponseWriter, responseCode int, message string) {
 	w.WriteHeader(responseCode)
 	w.Write([]byte(message))
+}
+
+func WriteJsonResponse(w http.ResponseWriter, responseCode int, message string, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseCode)
+	json.NewEncoder(w).Encode(data)
+
 }

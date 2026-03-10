2186aee65f14ed66384d7be906bcd3cba7153902
Brandon <95515854+brandon-kong@users.noreply.github.com>
2026-03-10 05:22:15 -0500

    feat: added api-client package to web app

--- apps/web/src/Home.tsx
+++ apps/web/src/Home.tsx
@@ -1,15 +1,23 @@
+"use client";
 
-import React from 'react';
+import { useState } from 'react';
+import { generateMagicLink } from '@auth/api-client';
 
 function Home() {
+    const [email, setEmail] = useState<string>("");
+
   return <div style={{
-    width: '100%',
-    height: '100%',
     display: 'flex',
     justifyContent:'center',
     alignItems:'center'
   }}>
     <h1>Welcome to react router dom and Query!</h1>
+    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+    <button
+    onClick={() => generateMagicLink(email)}
+    >
+        Test
+    </button>
   </div>;
 }
 

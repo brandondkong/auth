22dd2bf4cd6258854eb458ab1e7d7c8c535a3317
Brandon <95515854+brandon-kong@users.noreply.github.com>
2026-03-11 02:23:12 -0500

    feat: abstracted Hash method

---
+++ apps/api/internal/token/hash.go
@@ -0,0 +1,14 @@
+package token
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+)
+
+func HashString(str string) (string, error) {
+	hash := sha256.New()
+	hash.Write([]byte(str))
+
+	return base64.RawURLEncoding.EncodeToString(hash.Sum(nil)), nil
+}
+

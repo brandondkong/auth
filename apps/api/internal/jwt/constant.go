12b370f9d704f45b41a6ae3173b7b966d743b3f1
Brandon <95515854+brandon-kong@users.noreply.github.com>
2026-03-11 21:43:07 -0500

    refactor: ParseToken moved to jwtutils

--- apps/api/internal/jwt/constant.go
+++ apps/api/internal/jwt/constant.go
@@ -4,5 +4,6 @@ import "time"
 
 const TokenIssuer = "auth"
 
+const RefreshTokenCookie = "refresh"
 const RefreshTokenLifeTime = time.Hour * 24
 const AccessTokenLifeTime = time.Minute * 15
--- apps/api/internal/jwt/model.go
+++ apps/api/internal/jwt/model.go
@@ -1,20 +1,5 @@
 package jwt
 
-import (
-	"time"
-
-	"github.com/brandondkong/auth/internal/user"
-	"github.com/google/uuid"
-)
-
-type RefreshToken struct {
-	TokenId		string		`gorm:"primaryKey"`
-	ExpiresAt	time.Time
-	Revoked		*bool
-	UserId		uuid.UUID
-	User		user.User	`gorm:"foreignKey:UserId"`
-}
-
 type TokenPair struct {
 	Refresh		string	`json:"-"`
 	Access		string	`json:"access"`
